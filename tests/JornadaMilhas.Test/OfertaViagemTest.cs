770d7ad1713dcc2b7027557d172b332f8884e350
Matheus Wisch <wisch.pg@outlook.com>
2024-03-26 16:44:17 -0300

    feat: add testes

---
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -0,0 +1,13 @@
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test;
+
+public class OfertaViagemTest
+{
+    [Fact]
+    public void TestandoOfertaValida()
+    {
+        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+        Periodo periodo = new Periodo()
+    }
+}
