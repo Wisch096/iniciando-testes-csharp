e005bb5c314d9e0db44b057f22ee67831503e8b6
Matheus Wisch <wisch.pg@outlook.com>
2024-03-26 16:51:17 -0300

    feat: criando testes de construtor

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -8,6 +8,13 @@ public class OfertaViagemTest
     public void TestandoOfertaValida()
     {
         Rota rota = new Rota("OrigemTeste", "DestinoTeste");
-        Periodo periodo = new Periodo()
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), 
+            new DateTime(2024, 2, 5));
+        double preco = 100.0;
+        var validacao = true;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+        
+        Assert.Equal(validacao, oferta.EhValido);
     }
 }
