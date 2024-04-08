ff38b4be230a389546cd15070b15bcb7be65beb3
Matheus Wisch <wisch.pg@outlook.com>
2024-04-08 10:35:14 -0300

    feat: conhecendo padrao AAA

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -17,4 +17,19 @@ public class OfertaViagemTest
         
         Assert.Equal(validacao, oferta.EhValido);
     }
+    
+    [Fact]
+    public void TestandoOfertaComRotaNula()
+    {
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), 
+            new DateTime(2024, 2, 5));
+        double preco = 100.0;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        Assert.Contains("A ofertra de viagem nao possui rota ou período válidos",
+            oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
+    }
 }
