d84942771c44ab0880bd55c9814f6d665b6f8c40
Matheus Wisch <wisch.pg@outlook.com>
2024-06-15 10:54:09 -0300

    feat: create global json and test shoult not give negative price

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -32,4 +32,17 @@ public class OfertaViagemTest
             oferta.Erros.Sumario);
         Assert.False(oferta.EhValido);
     }
+
+    [Fact]
+    public void TestandoPrecoNegativo()
+    {
+        Rota rota = new Rota("Origem1", "Destino1");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), 
+            new DateTime(2024, 2, 5));
+        double preco = -250;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+    }
 }
