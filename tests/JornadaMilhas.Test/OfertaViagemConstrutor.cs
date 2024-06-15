18bf91f5b3a9f9e648c1cf1e33476f6173387a8a
Matheus Wisch <wisch.pg@outlook.com>
2024-06-15 11:15:05 -0300

    feat :sparkles: applying theory

--- tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
+++ tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -1,29 +1,33 @@
 using JornadaMilhasV1.Modelos;
+using Microsoft.EntityFrameworkCore;
 
 namespace JornadaMilhas.Test;
 
 public class OfertaViagemConstrutor
 {
-    [Fact]
-    public void RetornaOfertaValidaQuandoDadosValidos()
+    [Theory]
+    [InlineData("", null, "2024-01-01", "2024-01-02", 0, false)]
+    [InlineData("OrigemTeste", "DestinoTeste", "2024-02-01", "2024-02-05", 100, true)]
+    [InlineData(null, "São Paulo", "2024-01-01", "2024-01-02", -1, false)]
+    [InlineData("Vitória", "São Paulo", "2024-01-01", "2024-01-01", 0, false)]
+    [InlineData("Rio de Janeiro", "São Paulo", "2024-01-01", "2024-01-02", -500, false)]
+    public void RetornaOfertaValidaQuandoDadosValidos(string origem, string destino, string dataIda,
+        string dataVolta, double preco, bool validacao)
     {
-        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
-        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), 
-            new DateTime(2024, 2, 5));
-        double preco = 100.0;
-        var validacao = true;
+        Rota rota = new Rota(origem, destino);
+        Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
 
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
-        
+
         Assert.Equal(validacao, oferta.EhValido);
     }
-    
+
     [Fact]
     public void RetornaMensagemDeErroDeRotaOuPeriodoInvalidadosQuandoRotaNula()
     {
         Rota rota = null;
         Periodo periodo = new Periodo(new DateTime(2024, 2, 1), 
-            new DateTime(2024, 2, 5));
+                new DateTime(2024, 2, 5));
         double preco = 100.0;
 
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
@@ -37,7 +41,7 @@ public class OfertaViagemConstrutor
     public void RetornaMensagemDeErroDePreçoInvalidadoQuandoPrecoMenorQueZero()
     {
         Rota rota = new Rota("Origem1", "Destino1");
-        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), 
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1),
             new DateTime(2024, 2, 5));
         double preco = -250;
 
