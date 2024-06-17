23d1938884432abc385e958d118ec64ac7877d90
Matheus Wisch <wisch.pg@outlook.com>
2024-06-17 20:38:48 -0300

    test: add mutation

--- tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -19,18 +19,20 @@ public class OfertaViagemDesconto
         Assert.Equal(precoComDesconto, oferta.Preco);
     }
     
-    [Fact]
-    public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+    [Theory]
+    [InlineData(120, 30)]
+    [InlineData(100, 30)]
+    public void RetornaDescontoMaximoQuandoValorDescontoMaiorOuIgualAoPreco
+        (double desconto, double precoComDesconto)
     {
         Rota rota = new Rota("OrigemA", "DestinoB");
         Periodo periodo = new Periodo(new DateTime(2024, 05, 01),
             new DateTime(2024, 05, 10));
         double precoOriginal = 100.00;
-        double desconto = 120.00;
-        double precoComDesconto = 30.00;
         OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
         
         oferta.Desconto = desconto; 
+        
         Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
     }
 }
