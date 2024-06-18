7ada1bd28e8142a5d21784e705a2beffb198673f
Matheus Wisch <wisch.pg@outlook.com>
2024-06-18 20:42:14 -0300

    feat: create more tests

---
+++ tests/JornadaMilhas.Test/GerenciadorDeOfertasRecuperaMaiorDesconto.cs
@@ -0,0 +1,33 @@
+﻿using JornadaMilhasV1.Gerencidor;
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test;
+
+public class GerenciadorDeOfertasRecuperaMaiorDesconto
+{
+    [Fact]
+    public void RetornaOfertaNulaQuandoListaEstaVazia()
+    {
+        var lista = new List<OfertaViagem>();
+        var gerenciador = new GerenciadorDeOfertas(lista);
+        Func<OfertaViagem, bool> filtro = o => o.Rota.Destino.Equals("São Paulo");
+        
+        var oferta = gerenciador.RecuperaMaiorDesconto(filtro);
+        
+        Assert.Null(oferta);
+    }
+    
+    [Fact] 
+    public void RetornaOfertaEspecificaQuandoDestinoSaoPauloEDesconto40()
+    {
+        var lista = new List<OfertaViagem>();
+        var gerenciador = new GerenciadorDeOfertas(lista);
+        Func<OfertaViagem, bool> filtro = o => o.Rota.Destino.Equals("São Paulo");
+        var precoEsperado = 40;
+        
+        var oferta = gerenciador.RecuperaMaiorDesconto(filtro);
+        
+        Assert.NotNull(oferta);
+        Assert.Equal(precoEsperado, oferta.Preco, 0.0001);
+    }
+}
